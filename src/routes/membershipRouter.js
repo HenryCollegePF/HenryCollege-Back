79f7517bf5a660f4f8e4c383e11b49b045e19f49
Alejandra Betancourt <feellingokay@gmail.com>
2023-03-31 10:10:27 -0500

    payment routes

--- src/routes/membershipRouter.js
+++ src/routes/membershipRouter.js
@@ -1,15 +1,10 @@
 const { Router } = require("express");
-const { sequelize: { models: { Membership } } } = require("../db");
+const { sequelize: { models: { Membership, Students } } } = require("../db");
 const membershipRouter = Router();
 
-// 1. GET que traiga todas las membresías y las devuelva ✅
-// 2. POST para que cree una nueva membresia y la asigne a un estudiante ✅
-// 3. UPDATE que permita modificar la fecha en que vence la membresia
-// 4. DELETE que borre membresias
-
 // Controllers -> Maneja la logica
-const getMemberships = async () => { // Una promesa
-  const membership = await Membership.findAll()
+const getMemberships = async () => { 
+  const membership = await Membership.findAll({ include: Students })
 
   return membership
 }
@@ -27,29 +22,82 @@ const createMembership = async (values) => {
     pricePaid: pricePaid,
     paymentId: paymentId,
     expirationDate: expirationDate,
-    userId: userId,
+    StudentId: userId,
   })
 
   return newMembership
 }
 
 const updateMembership = async (values) => {
+  const { userId, ...propiedadesAModificar } = values
+  const user = await Students.findByPk(userId)
+  if (!user) throw new Error(`No se encontro el estudiante con el Id ${userId}`)
 
+  const studentSuscription = await user.getMembership()
+  if (!studentSuscription) throw new Error("El usuario no tiene una suscripción activa")
+
+  // Update
+  await studentSuscription.update(propiedadesAModificar)
+
+  return studentSuscription
+}
+
+const removeMembership = async (userId) => {
+  const user = await Students.findByPk(userId)
+  if (!user) throw new Error(`No se encontro el estudiante con el Id ${userId}`)
+
+  const studentSuscription = await user.getMembership()
+  if (!studentSuscription) throw new Error("El usuario no tiene una suscripción activa")
+
+  await studentSuscription.destroy()
 }
 
 // Handlers -> Le responde al front con res.send
 const getMembershipHandler = async (req, res) => {
-  const memberships = await getMemberships();
-  res.send(memberships)
+  try {
+    const memberships = await getMemberships();
+    res.send(memberships)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error.message)
+  }
 }
 
 const postMembershipHandler = async (req, res) => {
-  const postMembership = await createMembership(req.body);
-  res.send(postMembership)
+  try {
+    const postMembership = await createMembership(req.body);
+    res.send(postMembership)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error.message)
+  }
+}
+
+const updateMembershipHandler = async (req,res) => {
+  try {
+    const editMembership = await updateMembership(req.body);
+    res.send(editMembership)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error.message)
+  }
+}
+
+const removeMembershipHandler = async (req,res) => {
+  try {
+    const id = Number.parseInt(req.query.userId)
+    await removeMembership(id);
+    res.send("la membresía fue eliminada con éxito")
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message)
+  }
 }
 
 // Routear los handlers
-membershipRouter.get("/membership", getMembershipHandler)
-membershipRouter.post("/membership", postMembershipHandler)
+membershipRouter.get("/", getMembershipHandler)
+membershipRouter.post("/", postMembershipHandler)
+membershipRouter.put("/", updateMembershipHandler)
+membershipRouter.delete("/", removeMembershipHandler)
 
 module.exports = membershipRouter;
