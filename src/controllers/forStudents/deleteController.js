dda22fc23a4d194f6bbadf9f0721eb05b4dde0e5
= <=>
2023-04-04 01:22:02 -0300

    active

--- src/controllers/forStudents/deleteController.js
+++ src/controllers/forStudents/deleteController.js
@@ -1,8 +1,15 @@
 const {Students} = require("../../db")
 
 const deleteController = async(id) => {
- const db = await Students.update({active:false},{where:{id}})
- return "successfully erased"
+const student = await Students.findByPk(id)
+ if(student.active === false){
+ const active = await Students.update({active:true},{where:{id}})
+ return "it was activated successfully"
+ }else{
+    const db = await Students.update({active:false},{where:{id}})
+    return "successfully erased"
+ }
+ 
 }
 
 module.exports = deleteController
