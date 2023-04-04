7fa2cee2e400fbdee5e091741597c3c039b0a234
Alejandra Betancourt <feellingokay@gmail.com>
2023-04-03 21:25:10 -0500

    se agrega ruta para cambio de contraseña de estudiante, se borra dependencia que se habia instalado

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,20 +3,14 @@ const router = Router()
 const courseRouter = require("./courseRouter")
 const studentsRouter = require("./studentsRouter")
 const teachersRouter = require("./teachersRouter")
-<<<<<<< HEAD
 const membershipRouter = require("./membershipRouter")
-=======
 const feedBackRouter = require("./feedbackRouter")
->>>>>>> deleteRoutes
 
 // Rutas protegidas
 router.use("/course", courseRouter)
 router.use("/students", studentsRouter)
 router.use("/teachers", teachersRouter)
-<<<<<<< HEAD
 router.use("/membership", membershipRouter)
-=======
 router.use("/feedback", feedBackRouter)
->>>>>>> deleteRoutes
 
 module.exports = router; 
